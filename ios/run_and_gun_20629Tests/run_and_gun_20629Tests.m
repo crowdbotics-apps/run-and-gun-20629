27a347991c8b779d63392d2db3dc0d4cc839ba5d
Crowdbotics <team@crowdbotics.com>
2020-09-24 13:45:54 +0000

    Initial commit.

---
+++ ios/run_and_gun_20629Tests/run_and_gun_20629Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface run_and_gun_20629 : XCTestCase
+
+@end
+
+@implementation run_and_gun_20629
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
